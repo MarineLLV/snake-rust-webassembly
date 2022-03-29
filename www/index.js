5e7df0dd341aba2f8586f329a04ba55ed99928fc
MarineLLV <marine.lucottelevisage@gmail.com>
2022-03-29 19:06:55 +0200

    init --> link www and pkg folders

--- www/index.js
+++ www/index.js
@@ -1,21 +1,6 @@
-async function init() {
-    const importObject = {
-        console: {
-            log: () => {
-                console.log("Just logging something!");
-            },
-            error: () => {
-                console.error("Error!");
-            }
-        }
-    }
+import init, { greet } from "snake_game";
 
-    const response = await fetch("sum.wasm");
-    const buffer = await response.arrayBuffer();
-    const wasm = await WebAssembly.instantiate(buffer, importObject);
-    const sumFunction = wasm.instance.exports.sum;
-    const result = sumFunction(200, 80);
-    console.log(result);
-}
-
-init();
+init().then(_ => {
+    greet("Marine");
+    console.log("Everything ok!");
+})
