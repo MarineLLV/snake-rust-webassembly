1f2062a4a939f9eee1f0ed6e627550c5dbfc1aef
MarineLLV <marine.lucottelevisage@gmail.com>
2022-03-29 22:54:51 +0200

    create Snake

--- src/lib.rs
+++ src/lib.rs
@@ -4,22 +4,44 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+// Create snake
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>
+}
+
+impl Snake {
+    fn new(spawn_index: usize) -> Snake {
+        Snake{
+            body: vec!(SnakeCell(spawn_index))
+        }
+    }
+}
+
 #[wasm_bindgen]
 pub struct World {
-    width: usize
+    width: usize,
+    snake: Snake,
 }
 
 #[wasm_bindgen]
 impl World {
     pub fn new() -> World {
         World {
-            width: 8
+            width: 8,
+            snake: Snake::new(10) // start index 10
         }
     }
 
     // create getter for width
     pub fn width(&self) -> usize {
         self.width
+    }
+
+    // snake to the world
+    pub fn snake_head_index(&self) -> usize {
+        self.snake.body[0].0
     }
 }
 
